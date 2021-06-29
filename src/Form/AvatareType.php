67b8a30dec13508ba298ce3ebf4fe1bc3c3c921c
HEMMER-FX <hemmerfrancoisxavier@gmail.com>
2021-06-29 22:42:59 +0200

    css

--- src/Form/AvatareType.php
+++ src/Form/AvatareType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Avatar;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,9 +14,20 @@ class AvatareType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('url')
-            ->add('name')
+            ->add('url', TextType::class, [
+                'attr' => [
+                    'class' => '.uk-input'
+                ]
+            ])
+            ->add('name', TextType::class, [
+                'attr' => [
+                    'class' => 'uk-input'
+                ]
+            ])
             ->add('user', HiddenType::class, [
+                'attr' => [
+                    "class" => "uk-input"
+                ]
                 
             ])
         ;
