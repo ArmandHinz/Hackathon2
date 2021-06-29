b43fc88d54982baabb137bbadab88bf3ee0c6f84
Mélissa Kintz <kntzmelissa@gmail.com>
2021-06-29 16:37:17 +0200

    front index start ùessage

--- src/Form/MessageSujetType.php
+++ src/Form/MessageSujetType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\MessageSujet;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,10 +16,10 @@ class MessageSujetType extends AbstractType
         $builder
             ->add(
                 'message',
-                TextType::class,
+                TextareaType::class,
                 [
                     'attr' => [
-                        'class' => 'uk-textarea uk-width-1-1',
+                        'class' => 'bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-100 h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white',
                     ]
                 ]
             );
