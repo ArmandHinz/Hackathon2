f3cc456190f2cbc2e66bbf65c3d93d877cea08b1
Klipfel-Nicolas <klipfelnicolas86@gmail.com>
2021-06-30 18:22:45 +0200

    new light for a new home

--- assets/js/gsap.js
+++ assets/js/gsap.js
@@ -6,7 +6,7 @@ tl.from('.parentContainer .containerSujet', {x:-1500, opacity: 0, stagger:0.1, d
     .from('.rightCome', {x: 2000, duration:.5});
 
 tl.to('.transitionPage .toUp', {y:100, stagger:.1, duration:.1, delay: 1})
-.to('.transitionPage', {x:-2000, opacity: 0, duration:.3})
+.to('.transitionPage', {x:-2000, opacity: 0, duration:.3}) 
 
 tl.from('.opac', {opacity: 0, duration: .5})
     
